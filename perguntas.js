e28dba2e6e78514caaeb263e8ceb70e0331ed46b
estevan3a <estevanferreira151@gmail.com>
2024-11-27 16:25:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital do  Brasil',
+    'Brasília'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual é o valor da multiplicação de 597x59?',
+    '35.223'
+)
+
+criaCartao(
+    'Quimica',
+    'qual é a formula do gas carbonico',
+    'CO₂'
+)
+
+criaCartao(
+    'Fisica',
+    'O que significa ∆ e?',
+    'o delta é frequentemente utilizado para representar a diferença entre dois valores'
+)
+
+criaCartao(
+    'Biologia',
+    'o que são celulas',
+    'unidades estruturais e funcionais dos seres vivos, sendo a menor parte deles com forma e função'
+)
+
+criaCartao(
+    'Portugues',
+    'Para que serve os verbos?',
+    'expressar ações, estados ou fenômenos da natureza'
+)
